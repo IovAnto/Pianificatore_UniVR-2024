a65422a8a9b95ba02d0feff1c439ec3c635baa77
Tommi B <116102756+tbimbato@users.noreply.github.com>
2024-05-12 21:02:02 +0000

    savataggio in array

--- ASM/Funzioni di prova/Print concatenata/00.s
+++ ASM/Funzioni di prova/Print concatenata/00.s
@@ -1,21 +1,31 @@
 .section .data
 filename:
-    .ascii "test.txt"    # Nome del file di testo da leggere
-fd:
-    .int 0               # File descriptor
+    .ascii "test.txt"
+filedescriptor:
+    .int 0
+buffer:
+    .space 1
 
-buffer: .string ""       # Spazio per il buffer di input
-newline: .byte 10        # Valore del simbolo di nuova linea
-lines: .int 0            # Numero di linee
+newline:
+    .byte 10 # Carattere di nuova linea
+
+virgola:
+    .byte 44 # Carattere di virgola
+
+index:
+    .int 0
+
+array:
+    .space 100
 
 .section .bss
 
 .section .text
-    .globl _start
+.globl _start
 
 # Apre il file
 _open:
-    mov $5, %eax        # syscall open
+    mov $5, %eax        # Syscall open
     mov $filename, %ebx # Nome del file
     mov $0, %ecx        # Modalità di apertura (O_RDONLY)
     int $0x80           # Interruzione del kernel
@@ -24,48 +34,72 @@ _open:
     cmp $0, %eax
     jl _exit
 
-    mov %eax, fd      # Salva il file descriptor in ebx
+    mov %eax, filedescriptor      # filedescriptor = risultato syscall from eax
 
-# Legge il file riga per riga
+# lettura file
 _read_loop:
-    mov $3, %eax        # syscall read
-    mov fd, %ebx        # File descriptor
-    mov $buffer, %ecx   # Buffer di input
-    mov $1, %edx        # Lunghezza massima
+    mov $3, %eax        # Syscall read
+    mov $filedescriptor, %ebx       # File descriptor
+    mov $buffer, %ecx   # Buffer
+    mov $1, %edx      # Numero di byte da leggere
     int $0x80           # Interruzione del kernel
 
-    cmp $0, %eax        # Controllo se ci sono errori o EOF
-    jle _close_file     # Se ci sono errori o EOF, chiudo il file
+    # Se c'è un errore o EOF esce
+    cmp $0, %eax
+    jl _close_file
+
+    # Se non ha letto nulla, esce
+    cmp $0, %eax
+    je _print_array
     
-    # Controllo se ho una nuova linea
-    movb buffer, %al    # copio il carattere dal buffer ad AL
-    cmpb newline, %al   # confronto AL con il carattere \n
-    jne _print_line     # se sono diversi stampo la linea
-    incw lines          # altrimenti, incremento il contatore
+    # Controllo se il carattere letto è un newline
+    movb buffer, %al    # copio il carattere dal buffer ad AL (registro a 8 bit di %eax)
+    cmpb newline, %al   # confronto AL con il carattere "\n"
 
-_print_line:
-    # Stampa il contenuto della riga
-    mov $4, %eax        # syscall write
-    mov $1, %ebx        # File descriptor standard output (stdout)
-    mov $buffer, %ecx   # Buffer di output
-    int $0x80           # Interruzione del kernel
+    jne _saveToArray    # se sono diversi salvo in array
 
-    jmp _read_loop      # Torna alla lettura del file
+    # Controllo se il carattere letto è una virgola
+    movb buffer, %al    # copio il carattere dal buffer ad AL (registro a 8 bit di %eax)
+    cmpb virgola, %al   # vedo se è virgola
+    jne _saveToArray    # se sono diversi salvo in array
+    
+    incw index          # altrimenti, incremento index array
 
-# Chiude il file
+
+_saveToArray:
+    movb buffer, %al    # copio il carattere dal buffer ad AL (registro a 8 bit di %eax)
+    mov index, %edx    # copio index in edx
+    movb %al, array(, %edx, 1) # salvo il carattere in array
+    incw index          # incremento index array
+
 _close_file:
-    mov $6, %eax        # syscall close
-    mov %ebx, %ecx      # File descriptor
-    int $0x80           # Interruzione del kernel
+    mov $6, %eax                # Syscall close
+    mov filedescriptor, %ebx    # File descriptor
+    int $0x80                   # Interruzione del kernel
+
+    jmp _exit
+
+
+    _print_array:
+        mov $4, %eax        # Syscall write
+        mov $1, %ebx        # File descriptor (stdout)
+        mov $array, %ecx    # Buffer
+        mov index, %edx     # Numero di byte da scrivere
+        int $0x80           # Interruzione del kernel
+        ret
+
+_print_array_loop:
+    cmpw $0, index     # Confronto index con 0
+    jle _exit          # Se index <= 0, esce
+    decw index         # Decrementa index
+    call _print_array  # Stampa array
+    jmp _print_array_loop
 
 _exit:
-    mov $1, %eax        # syscall exit
-    xor %ebx, %ebx      # Codice di uscita 0
+    jmp _print_array_loop
+    mov $1, %eax        # Syscall exit
+    mov $0, %ebx        # Codice di uscita
     int $0x80           # Interruzione del kernel
 
 _start:
-    jmp _open          # Chiama la funzione per aprire il file
-
-    # Fine programma
-    jmp _exit
-
+    call _open
