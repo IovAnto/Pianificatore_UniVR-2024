1f5ce438ef24549fb67410f44cbb2ce1a578e914
Tommi B <116102756+tbimbato@users.noreply.github.com>
2024-05-13 14:04:59 +0000

    . . .

--- ASM/Funzioni di prova/Print concatenata/bufferbig.s
+++ ASM/Funzioni di prova/Print concatenata/bufferbig.s
@@ -4,6 +4,8 @@ buffer:     .space 1024
 fd:         .int 0
 newline:    .asciz "\n"
 
+struct:      .space 400
+
 .section .text
 .global _start
 _start:
@@ -32,47 +34,15 @@ _start:
     cmp $0, %eax
     jl error            # se eax < 0, c'è stato un errore
 
-
-    # stampo il contenuto del file
-    mov $4, %eax        # syscall write
-    mov $1, %ebx        # file descriptor stdout
-    mov $buffer, %ecx   # buffer di sorgente
-    mov $256, %edx      # lunghezza del buffer
-    int $0x80           # chiamata di sistema
-    
-    # #############################################
-    # stampo 3 volte '\n'
-    mov $4, %eax        # syscall write
-    mov $1, %ebx        # file descriptor stdout
-    mov $newline, %ecx  # buffer di sorgente
-    mov $1, %edx        # lunghezza del buffer
-    int $0x80           # chiamata di sistema
-    # #############################################
-
-    # loop di stampa leggendo da 'buffer' stampando 1 carattere alla volta
-    mov $0, %esi        # inizializzo il contatore
-    .print_loop:
-
-        mov $4, %eax        # syscall write
-        mov $1, %ebx        # file descriptor stdout
-        mov $buffer, %ecx   # buffer di sorgente
-        add %esi, %ecx      # aggiungo il contatore al buffer
-        mov $1, %edx        # lunghezza del buffer
-        int $0x80           # chiamata di sistema
-
-        # incremento il contatore
-        inc %esi
-
-        # controllo se ho finito di stampare
-        cmp $1024, %esi
-        jl .print_loop
-
     # Chiude il file
     mov $6, %eax        # syscall close
     mov %ebx, %ebx      # file descriptor
     int $0x80           # chiamata di sistema
 
-    # Termina il programma
+    # popolo struct con dati che ho nel buffer:
+    
+
+    # Termina il programma -> sarebbe RET
     mov $1, %eax        # syscall exit
     xor %ebx, %ebx      # codice di uscita 0
     int $0x80           # chiamata di sistema
@@ -92,3 +62,6 @@ error:
 
 .section .data
 error_msg:  .asciz "Errore durante l'apertura del file\n"
+
+
+# struct: byte byte byte byte -> sizeof(struct) = 4 -> 100 prodotti = 400 Byte
