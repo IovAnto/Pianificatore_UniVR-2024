8d86dd8825f86599ad8bbebbcf998c761a564e37
tbimbato <116102756+tbimbato@users.noreply.github.com>
2024-05-17 17:09:10 +0200

    refactor: Update QuickSort assembly code to improve readability and add comments

--- ASM/Funzioni di prova/test04.1_QuickSortSOLO/QuickSortLauncher.s
+++ ASM/Funzioni di prova/test04.1_QuickSortSOLO/QuickSortLauncher.s
@@ -4,6 +4,7 @@
 pointer: .int 0
 high: .int 0
 low: .int 0
+pivot: .int 0 # forse non serve
 
 
 .section .text
@@ -11,18 +12,54 @@ low: .int 0
 .type QuickSort, @function
 
 QuickSort:
+
+
+#   {  //arg= puntatore ad array, low=indice iniziale, high=indice finale
+#       if (low < high) {                           //se l'indice iniziale è minore di quello finale
+#           int pi = partition(arr, low, high);     //pi è l'indice del pivot (partiziona l'array e restituisce l'indice del pivot)
+#           quickSort(arr, low, pi - 1);            //ricorsione per la parte sinistra dell'array
+#           quickSort(arr, pi + 1, high);           //ricorsione per la parte destra dell'array
+#       }
+#   }
     
+    # mi preparo low e high e il puntatore ad array:
     movl %esi, pointer
     movl %ebx, low
     movl %edi, high
     
-
+    # comparo low < high
     cmpl %edi, %ebx
     jge end
 
-    call partition
-    #metto le giuste variabili nei registri
+    # chiamo partition per trovare il pivot e lo salvo
+        call partition  # il pivot è in %dl
 
+        movl %eax, pivot
+
+
+        movl pointer, %esi
+        movl low, %ebx
+        movl pivot, %edi
+        addl $-4, %edi
+
+        call QuickSort
+
+        movl %eax, pivot
+
+        movl pointer, %esi
+        movl pivot, %ebx
+        movl high, %edi
+        addl $4, %ebx
+        
+        call QuickSort
+
+    # chiamo ricorsivamente QuickSort per la parte sinistra dell'array
+    # preparo regitstri con gli argomenti giusti:
+    # puntatore ad array
+    # puntatore a array[low]
+    # puntatore a array[pivot-1]
+
+    
     #ricorsivo
 
     call QuickSort
