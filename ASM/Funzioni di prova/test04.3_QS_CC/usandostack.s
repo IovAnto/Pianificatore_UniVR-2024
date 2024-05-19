2dbd3f17d7f413914f8b732386b54b09b3064e38
tbimbato <116102756+tbimbato@users.noreply.github.com>
2024-05-19 19:39:05 +0200

    . . .

--- ASM/Funzioni di prova/test04.3_QS_CC/usandostack.s
+++ ASM/Funzioni di prova/test04.3_QS_CC/usandostack.s
@@ -1,89 +1,80 @@
 .section .data
-pointer: .int 0
-high: .int 0
-low: .int 0
-pivot: .int 0
-i: .int 0
-j: .int 0
-pindex: .int 0
+pointer: .int 0    ; Variabile per il puntatore all'array
+high: .int 0       ; Variabile per l'indice più alto dell'array
+low: .int 0        ; Variabile per l'indice più basso dell'array
+pivot: .int 0      ; Variabile per il valore del pivot
+i: .int 0          ; Variabile per l'indice i
+j: .int 0          ; Variabile per l'indice j
+pindex: .int 0     ; Variabile per l'indice del pivot
 
 .section .text
 .global QuickSort
 .type QuickSort, @function
 
-# arr = EAX
-# low = ECX
-# high = EDX
-
 QuickSort:
-    pushl %ebp
-    movl %esp, %ebp
-    pushl %ebx
-    pushl %esi
-    pushl %edi
+    pushl %ebp      ; Salva il valore di ebp nello stack
+    movl %esp, %ebp ; Imposta ebp come il nuovo valore di esp
+    pushl %ebx      ; Salva il valore di ebx nello stack
+    pushl %esi      ; Salva il valore di esi nello stack
+    pushl %edi      ; Salva il valore di edi nello stack
 
-    # if low < high
-    cmpl %ecx, %edx
-    jge end_quick_sort
+    cmpl %ecx, %edx ; Confronta low con high
+    jge end_quick_sort ; Salta alla fine se low >= high
 
-    # Partition logic
-    # pivot = arr[high]
-    movl (%eax, %edx, 4), %ebx
-    movl %ebx, pivot
+    movl (%eax, %edx, 4), %ebx ; Carica il valore di arr[high] in ebx
+    movl %ebx, pivot ; Salva il valore di arr[high] in pivot
 
-    # i = low - 1
-    movl %ecx, %esi
-    subl $1, %esi
-    movl %esi, i
+    movl %ecx, %esi ; Salva il valore di low - 1 in esi
+    subl $1, %esi   ; Sottrae 1 da esi
+    movl %esi, i    ; Salva il valore di esi in i
 
-    movl %ecx, %edi  # j = low
+    movl %ecx, %edi ; Salva il valore di low in edi
 
 partition_loop:
-    cmpl %edx, %edi
-    jge end_partition_loop
+    cmpl %edx, %edi ; Confronta j con high
+    jge end_partition_loop ; Salta alla fine del loop se j >= high
 
-    movl (%eax, %edi, 4), %ebx
-    cmpl pivot, %ebx
-    jg skip_swap
+    movl (%eax, %edi, 4), %ebx ; Carica il valore di arr[j] in ebx
+    cmpl pivot, %ebx ; Confronta pivot con ebx
+    jg skip_swap ; Salta allo step successivo se pivot > ebx
 
-    addl $1, i
-    movl i, %esi
-    movl (%eax, %esi, 4), %ebx
-    movl (%eax, %edi, 4), %ecx
-    movl %ecx, (%eax, %esi, 4)
-    movl %ebx, (%eax, %edi, 4)
+    addl $1, i ; Incrementa i di 1
+    movl i, %esi ; Salva il valore di i in esi
+    movl (%eax, %esi, 4), %ebx ; Carica il valore di arr[i] in ebx
+    movl (%eax, %edi, 4), %ecx ; Carica il valore di arr[j] in ecx
+    movl %ecx, (%eax, %esi, 4) ; Salva il valore di arr[j] in arr[i]
+    movl %ebx, (%eax, %edi, 4) ; Salva il valore di arr[i] in arr[j]
 
 skip_swap:
-    addl $1, %edi
-    jmp partition_loop
+    addl $1, %edi ; Incrementa j di 1
+    jmp partition_loop ; Salta all'inizio del loop
 
 end_partition_loop:
-    addl $1, i
-    movl i, %esi
-    movl (%eax, %esi, 4), %ebx
-    movl (%eax, %edx, 4), %ecx
-    movl %ecx, (%eax, %esi, 4)
-    movl %ebx, (%eax, %edx, 4)
+    addl $1, i ; Incrementa i di 1
+    movl i, %esi ; Salva il valore di i in esi
+    movl (%eax, %esi, 4), %ebx ; Carica il valore di arr[i] in ebx
+    movl (%eax, %edx, 4), %ecx ; Carica il valore di arr[high] in ecx
+    movl %ecx, (%eax, %esi, 4) ; Salva il valore di arr[high] in arr[i]
+    movl %ebx, (%eax, %edx, 4) ; Salva il valore di arr[i] in arr[high]
 
-    movl i, pindex
+    movl i, pindex ; Salva il valore di i in pindex
 
-    # Recursive calls
-    movl %eax, %ebx
-    movl %ecx, %ecx
-    movl pindex, %edx
-    subl $1, %edx
-    call QuickSort
+    movl %eax, %ebx ; Salva il valore di arr in ebx
+    movl %ecx, %ecx ; Salva il valore di high in ecx
+    movl pindex, %edx ; Salva il valore di pindex in edx
+    subl $1, %edx ; Sottrae 1 da edx
+    call QuickSort ; Chiama QuickSort ricorsivamente
 
-    movl %eax, %ebx
-    movl pindex, %ecx
-    addl $1, %ecx
-    movl high, %edx
-    call QuickSort
+    movl %eax, %ebx ; Salva il valore di arr in ebx
+    movl pindex, %ecx ; Salva il valore di pindex in ecx
+    addl $1, %ecx ; Incrementa ecx di 1
+    movl high, %edx ; Salva il valore di high in edx
+    call QuickSort ; Chiama QuickSort ricorsivamente
 
 end_quick_sort:
-    popl %edi
-    popl %esi
-    popl %ebx
-    movl %ebp, %esp
-    popl %ebp
-    ret
+    popl %edi ; Ripristina il valore di edi dallo stack
+    popl %esi ; Ripristina il valore di esi dallo stack
+    popl %ebx ; Ripristina il valore di ebx dallo stack
+    movl %ebp, %esp ; Ripristina il valore di esp da ebp
+    popl %ebp ; Ripristina il valore di ebp dallo stack
+    ret ; Ritorna dalla funzione
