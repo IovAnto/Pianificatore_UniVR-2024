190e7e854585ec27cab50a1c99c98c6b34707288
tbimbato <116102756+tbimbato@users.noreply.github.com>
2024-05-24 12:07:10 +0200

    . . .

--- ASM/Funzioni di prova/test09_SuperSortedArray/EqualSort.s
+++ ASM/Funzioni di prova/test09_SuperSortedArray/EqualSort.s
@@ -1,7 +1,7 @@
 .section .data
 
-base: .byte 0
-flag: .byte 0
+base: .byte 0               # EDF or HPF
+flag: .byte 0               # 1 -> swap 0 -> no swap
 
 .section .text
     .global EqualSort
@@ -9,55 +9,55 @@ flag: .byte 0
 
 EqualSort:
 
-    pushl %ebp
+    pushl %ebp              
     movl %esp, %ebp
  
-    movl 8(%ebp), %edi #len
-    movl 12(%ebp), %edx #pointer
-    movl 16(%ebp), %ecx #Base
+    movl 8(%ebp), %edi      #len
+    movl 12(%ebp), %esi     #pointer
+    movl 16(%ebp), %ecx     #Base
 
-    popl %ebp
+    popl %ebp               #restore stack
 
-    movb %cl, base
-    movl %ecx, %eax
-    movl %eax, %ebx
-    addl $4, %ebx
+    movb %cl, base          #salvo base (EDF or HPF)
+    movl %ecx, %eax         # i
+    movl %eax, %ebx         # j
+    addl $4, %ebx           # j + 1
 
 loop:
 
-    cmpl %eax, %edi
+    cmpl %eax, %edi         #len <= i (uscita)
     jle endLoop
 
     xorl %ecx, %ecx
     xorl %edx, %edx
 
-    movb (%esi, %eax, 1), %cl
+    movb (%esi, %eax, 1), %cl   
     movb (%esi, %ebx, 1), %dl
 
-    cmpb %cl, %dl
-    je Check
+    cmpb %cl, %dl               #confronto i e j
+    je Check                    #se sono uguali vado a check
 
     jmp next
 
 Check:
 
-    cmpb $3, base
+    cmpb $3, base               #se base = HPF (priorità) -> decrement
     je decrement
 
-increment:
+increment:                      # guardo priorità
 
-    incl %eax
-    incl %ebx
+    incl %eax                   #incremento i
+    incl %ebx                   #incremento j
 
-    movb (%esi, %eax, 1), %cl
+    movb (%esi, %eax, 1), %cl       # preparo il confronto dei prossimi valori
     movb (%esi, %ebx, 1), %dl
 
 
-    subl $3, %eax
-    subl $3, %ebx
+    subl $3, %eax           # torno a puntare l'inizio dell'elemento
+    subl $3, %ebx           # =
 
-    cmpb %cl, %dl
-    jg reset
+    cmpb %cl, %dl           # confronto i e j
+    jg reset                
     je SetDurata
 
     movl (%esi, %eax, 1), %ecx
@@ -70,7 +70,7 @@ increment:
 
     jmp reset
 
-decrement:
+decrement:                  # guardo scadenza
 
     decl %eax
     decl %ebx
@@ -149,3 +149,12 @@ endLoop:
     
     ret
     
+
+
+    # appuntino :-) 
+    # If you're dealing with a big endian system and you need to convert data to little endian,
+    # you'll have to write code to swap the byte order manually. Here's an example of how you can do this for a 32-bit word:
+
+    # Assume that %eax contains the value to be converted
+    # ror $16, %eax     # Swap the two 16-bit halves of EAX
+    # bswap %eax        # Swap the bytes within each 16-bit half
